974f887b31e539292282066c85bfcbb648a3926e
allen <revenoir7@gmail.com>
2019-06-07 09:35:34 +0900

    refactor: Seperate some functions to add options

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function getOriginResult(totalTime) {
+  return `총 ${totalTime / 60 / 60 | 0}시간 ${totalTime / 60 % 60 | 0}분`
+}
+
+function getRatedResult(rate, totalTime) {
+  return `${totalTime / rate / 60 / 60 | 0}시간 ${totalTime / rate / 60 % 60 | 0}분`
+}
+
 function click() {
   chrome.tabs.executeScript({ file: 'content_script.js' }, function (result) {
-
-    document.querySelector('#result').innerText = `총 ${result / 60 / 60 | 0}시간 ${result / 60 % 60 | 0}분`;
-    document.querySelector('.ratedResult').innerText = `1.25배속 시 : ${result / 1.25 / 60 / 60 | 0}시간 ${result / 1.25 / 60 % 60 | 0}분`;
+    document.querySelector('#result').innerText = getOriginResult(result);
+    document.querySelector('.ratedResult').innerText = `1.25배속 시 : ` + getRatedResult(1.25, result);
   })
 }
 
